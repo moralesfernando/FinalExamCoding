e47f7425d306b4bd95aa5c832b0b1ef9da8a3293
moral <moral@DESKTOP-TPSHLG8>
2016-12-07 03:47:47 -0500

    Not quite on Unit tests

--- RocketDAL/src/test/java/rocketBase/Rate_Test.java
+++ RocketDAL/src/test/java/rocketBase/Rate_Test.java
@@ -9,22 +9,21 @@ import org.junit.Test;
 import rocketDomain.RateDomainModel;
 
 public class Rate_Test {
-
-	
-	//TODO - RocketDAL rate_test
-	//		Check to see if a known credit score returns a known interest rate
-	
-	//TODO - RocketDAL rate_test
-	//		Check to see if a RateException is thrown if there are no rates for a given
-	//		credit score
 	@Test
 	public void test() {
 		
-		ArrayList<RateDomainModel> rates = RateDAL.getAllRates();
-		System.out.println ("Rates size: " + rates.size());
-		assert(rates.size() > 0);
+		ArrayList<RateDomainModel> ALLrates = RateDAL.getAllRates();
 		
-		assert(1==1);
+		assertEquals(ALLrates.size(),5);
+		
+		for(RateDomainModel RDM : ALLrates){
+			System.out.println (RDM);
+		}
+		
+		assertEquals(ALLrates.get(0).getiRateID(),1);
+		assertEquals(ALLrates.get(0).getdInterestRate(),5);
+		assertEquals(ALLrates.get(0).getiMinCreditScore(),600);
+		
 	}
 
 }
